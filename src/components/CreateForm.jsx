12f9d03d6bfecfb2dd12d5410c64b23d2dbe1b8e
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2025-08-28 01:00:44 +0300

    Added better usability in tailwindcss

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -15,22 +15,31 @@ export default function CreateForm({
     return (
         <section className="create-container 
         w-full h-full relative flex flex-col items-center justify-center">
-            <article className="w-[570px] h-[437px] bg-gradient-to-b from-[#29de00] to-[#003d1a] rounded-[50px]">
-                <div className="w-[457px] h-[77px] bg-[#d9d9d9] rounded-[20px]" />
-                <div className="w-[453px] h-[70px] bg-[#d9d9d9] rounded-[20px]" />
-                <div className="w-[170px] h-[18px] text-center justify-start text-[#454545] text-2xl font-semibold font-['Poppins']">Lobby name</div>
-                <div className="w-[170px] h-[18px] text-center justify-start text-[#454545] text-2xl font-semibold font-['Poppins']">Player name</div>
+            <form className="w-sm h-1/2 flex flex-col bg-gradient-to-b from-secondary-light to-secondary-dark rounded-[50px]">
+
+                <label className=" justify-start text-primary-light text-2xl font-semibold font-['Poppins']">Lobby name</label>
+                <input className="w-[457px] h-[77px] bg-[#d9d9d9] rounded-[20px]"
+                    type="text"
+                    placeholder="Enter your name"
+                    value={playerName}
+                    onChange={(e) => setPlayerName(e.target.value)}
+                    required />
+
+                <label className="w-[170px] h-[18px] text-center justify-start text-primary-light text-2xl font-semibold font-['Poppins']">Player name</label>
+                <input className="w-[453px] h-[70px] bg-[#d9d9d9] rounded-[20px]"
+                    type="text"
+                    placeholder="Enter your name" value={lobbyName}
+                    onChange={(e) => setLobbyName(e.target.value)} />
+
                 <section className="button-wrapper flex flex-row gap-10">
-                    <div className="w-[204px] h-[78px] rounded-[50px] overflow-hidden">
-                        <div className="w-[203px] h-[71px] bg-gradient-to-l from-[#272727] to-[#464545] rounded-[40px] shadow-[inset_0px_4px_12px_0px_rgba(0,0,0,0.20)]" />
-                        <div className="w-[170px] h-9 text-center justify-start text-white text-2xl font-semibold font-['Poppins']">Create game</div>
-                    </div>
-                    <div className="w-[204px] h-[78px] rounded-[50px] overflow-hidden">
-                        <div className="w-[203px] h-[71px] bg-gradient-to-b from-[#464545] to-[#272727] rounded-[40px] shadow-[inset_0px_4px_12px_0px_rgba(0,0,0,0.20)]" />
-                        <div className="w-[170px] h-9 text-center justify-start text-white text-2xl font-semibold font-['Poppins']">Back<br /></div>
-                    </div>
+                    <button className="w-[203px] h-[71px] bg-gradient-to-l from-[#272727] to-[#464545] rounded-[40px] shadow-[inset_0px_4px_12px_0px_rgba(0,0,0,0.20)]">
+                        <p className="w-[170px] h-9 text-center justify-start text-white text-2xl font-semibold font-['Poppins']">Create game</p>
+                    </button>
+                    <button className="w-[203px] h-[71px] bg-gradient-to-b from-[#464545] to-[#272727] rounded-[40px] shadow-[inset_0px_4px_12px_0px_rgba(0,0,0,0.20)]">
+                        <p className="w-[170px] h-9 text-center justify-start text-white text-2xl font-semibold font-['Poppins']">Back<br /></p>
+                    </button>
                 </section>
-            </article>
+            </form>
         </section>
     );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,28 +4,28 @@ export default function Welcome({setView}) {
         <section className="welcome-container relative
         w-full h-full 
         flex flex-col items-center justify-center 
-        bg-gradient-to-l from-neutral-800 to-[#454545] 
+        bg-gradient-to-l from-primary-dark to-primary-light 
         ">
             <section className="flex flex-col gap-2 p-10 w-full text-center justify-start">
                 <section>
                     <h1 className="main-heading text-[75px] text-white font-semibold font-['Poppins']">
-                        Play <span className="inline-block bg-gradient-to-b from-[#003d1a] to-[#29de00] bg-clip-text text-transparent 
+                        Play <span className="inline-block bg-gradient-to-b from-secondary-dark to-secondary-light bg-clip-text text-transparent 
                     text-[75px] font-semibold font-['Poppins']"> Belote </span> online
                     </h1>
                     <h1 className="main-heading text-[75px] text-white font-semibold font-['Poppins']"> free with friends</h1>
                 </section>
                 <section className="buttons-section w-full flex flex-row items-center justify-center gap-10">
-                    <button onClick={() => setView('create')} className="cursor-pointer p-4 bg-gradient-to-bl from-[#29de00] to-[#003d1a] rounded-[40px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.20)]">
+                    <button onClick={() => setView('create')} className="cursor-pointer p-4 bg-gradient-to-bl from-secondary-light to-secondary-dark rounded-[40px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.20)]">
                         <p className="text-center text-white text-2xl font-semibold font-['Poppins']">Create game</p>
                     </button>
-                    <button onClick={() => setView('join')} className="cursor-pointer p-4 pl-10 pr-10 bg-gradient-to-b from-[#29de00]/90 to-[#003d1a] rounded-[40px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.20)]">
+                    <button onClick={() => setView('join')} className="cursor-pointer p-4 pl-10 pr-10 bg-gradient-to-b from-secondary-light/90 to-secondary-dark rounded-[40px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.20)]">
                         <p className="text-center text-white text-2xl font-semibold font-['Poppins']">Join game</p>
                     </button>
                 </section>
             </section>
 
             <section className="green-container flex flex-row gap-10
-            w-full h-full bg-gradient-to-br from-[#29de00] to-[#003d1a] shadow-[4px_0px_5px_0px_rgba(255,0,0,0.25)] p-20">
+            w-full h-full bg-gradient-to-br from-secondary-light to-secondary-dark shadow-[4px_0px_5px_0px_rgba(255,0,0,0.25)] p-20">
                 <p className="text-center justify-center text-white text-[41px] font-semibold font-['Poppins'] leading-[45px]">
                     Az shte sum konkurentsiq na Belot.bg. Ako ne vi se davat pari i vi se
                     tsuka s priqteli ste na pravilnoto mqsto! Are mazna i priqtna igra!
